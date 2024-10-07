3fd7a99466e4ca5c2c33485f1fbc2a8b1abd69bc
TheGoldenChicken <karlmeisner99@gmail.com>
2024-10-07 14:01:20 +0200

    add material for week 6

---
+++ week_6/titles.py
@@ -0,0 +1,4 @@
+all_titles = ["Juicy Kylling by Grunken", "Getting John Hutcherson'd", "Monday Left me Broken", "Don Giovanni - Commodore Scene",
+"Queen of the Night Aria", "Free Bird Guitar Solo", "Davie504 'Epic Bass Solo'",
+"A rooster", "Karl, a rooster", "Too much brainrot"]
+
